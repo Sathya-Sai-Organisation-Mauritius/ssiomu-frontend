c42b7b8e5001e26c4450453ad2792032f2fea90e
Mahima Ramgolam <mahima@ramgolam.com>
2020-06-19 12:33:10 +0400

    region GET added

--- components/RegionsComponentBox.vue
+++ components/RegionsComponentBox.vue
@@ -3,47 +3,49 @@
     <div
       class="views-row bg-white  px-4 pt-2 rounded shadow-lg border-b-4 border-blue-700 "
     >
-      <div class="flex items-center grid grid-cols-5 gap-4">
-        <div
-          class=" circ-num text-md md:text-xl lg:text-4xl text-center sm:font-medium text-blue-600"
-        >
-          <span class="">#</span>
-          {{ myvalues.number }}
-        </div>
-
-        <div
-          class="flex col-span-3 font-semibold text-sm md:text-xl lg:text-3xl text-blue-900"
-        >
-          <nuxt-link class="hover:underline" :to="myvalues.url">
-            {{ myvalues.region }}</nuxt-link
-          >
-        </div>
-        <div class="flex justify-end">
+      <nuxt-link class="hover:underline" :to="'/region/' + myvalues.id">
+        <div class="flex items-center grid grid-cols-5 gap-4">
           <div
-            class="shape-wrapper relative w-8 h-8 md:w-16 md:h-16"
-            style="--map-size:50px;"
+            class=" circ-num text-md md:text-xl lg:text-4xl text-center sm:font-medium text-blue-600"
           >
-            <div class="shape absolute ">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="0.5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                class="feather feather-map "
-              >
-                <polygon
-                  points="1 6 1 20 8 16 16 20 23 16 23 2 16 6 8 2 1 6"
-                ></polygon>
-                <line x1="8" y1="2" x2="8" y2="16"></line>
-                <line x1="16" y1="6" x2="16" y2="20"></line>
-              </svg>
+            <span class="">#</span>
+            {{ myvalues.number }}
+          </div>
+
+          <div
+            class="flex col-span-3 font-semibold text-sm md:text-xl lg:text-3xl text-blue-900"
+          >
+            <!-- <nuxt-link class="hover:underline" :to="myvalues.url"> -->
+            {{ myvalues.name }}
+            <!-- </nuxt-link > -->
+          </div>
+          <div class="flex justify-end">
+            <div
+              class="shape-wrapper relative w-8 h-8 md:w-16 md:h-16"
+              style="--map-size:50px;"
+            >
+              <div class="shape absolute ">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="0.5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="feather feather-map "
+                >
+                  <polygon
+                    points="1 6 1 20 8 16 16 20 23 16 23 2 16 6 8 2 1 6"
+                  ></polygon>
+                  <line x1="8" y1="2" x2="8" y2="16"></line>
+                  <line x1="16" y1="6" x2="16" y2="20"></line>
+                </svg>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </nuxt-link>
     </div>
   </div>
 </template>
---
+++ pages/region/_id.vue
@@ -0,0 +1,110 @@
+<template>
+  <div>
+    <SiteHeader />
+
+    <div class="container mx-auto">
+      <div class="region-details grid gap-1 grid-cols-2 space-y-12 py-10">
+        <div
+          class="region-title font-bold flex col-span-1 space-x-2 items-center"
+        >
+          <h1 class="font-serif text-5xl">
+            Lower Plaine Wilhems {{ myResult.name }}
+          </h1>
+        </div>
+        <div class="relative col-span-1" style="margin-top: 0;">
+          <div
+            class="absolute top-0 right-0 text-2xl text-gray-700 bg-black-100 border-2 border-black-900 rounded-full p-3 px-10 font-black"
+          >
+            Region 5
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <UpcomingEvents :color="'gradient-bg'" :textcolor="'text-blue-600'" />
+
+    <PastEvents />
+
+    <FeaturedPublications />
+
+    <OfficeBearer />
+
+    <SiteFooter />
+  </div>
+</template>
+
+<script>
+import SiteHeader from '~/components/SiteHeader.vue'
+import SiteFooter from '~/components/SiteFooter.vue'
+import UpcomingEvents from '~/components/UpcomingEvents.vue'
+import FeaturedPublications from '~/components/FeaturedPublications.vue'
+import PastEvents from '~/components/PastEvents.vue'
+
+export default {
+  data: () => {
+    return {
+      subtitle: 'A list of events in region X '
+    }
+  },
+  components: {
+    SiteHeader,
+    SiteFooter,
+    UpcomingEvents,
+    FeaturedPublications,
+    PastEvents
+  },
+
+  data() {
+    return {
+      pageId: this.$route.params.id,
+      myResult: false,
+      errors: false
+    }
+  },
+
+  methods: {
+    getJson(response) {
+      return response.json()
+    },
+
+    displayData(result) {
+      console.log(result)
+      this.myResult = result.data
+    },
+
+    handleError(error) {
+      console.log(error)
+      this.errors = 'An error occured. Please try again later.'
+    },
+
+    formatDate(param) {
+      let temporaryDate = new Date(param)
+      let month = temporaryDate.toLocaleString('default', { month: 'long' })
+      let day = temporaryDate.toLocaleString('default', { day: 'numeric' })
+      let year = temporaryDate.toLocaleString('default', { year: 'numeric' })
+
+      let fullDate = `${month} ${day}, ${year}`
+      return fullDate
+    },
+
+    fetchData() {
+      fetch(
+        'http://localhost:4444/_/items/region/' + this.pageId + '?fields=*.*'
+      )
+        .then(this.getJson)
+
+        .then(this.displayData)
+        .catch(this.handleError)
+    }
+  },
+  mounted() {
+    this.fetchData()
+  }
+}
+</script>
+
+<style>
+.gradient-bg {
+  background: linear-gradient(90deg, #1c5877, #0d2d3d 100%);
+}
+</style>
