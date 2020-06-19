2448b891056b4c026856339332e99f21d858685a
Sandeep Ramgolam <sandeep@ramgolam.com>
2020-06-19 19:27:16 +0400

    breaking news styles

--- components/BreakingNews.vue
+++ components/BreakingNews.vue
@@ -3,21 +3,16 @@
     <div
       class="container mx-auto px-4 md:px-0 py-10 flex flex-col justify-center "
     >
-      <div class="md:flex justify-between bg-red-100 items-center ">
+      <div class="md:flex justify-between bg-red-100 items-center">
         <div
-          class=" bg-red-700 text-center font-bold text-white p-2 md:p-4 text-xs md:text-xl lg:text-2xl leading-none"
+          class=" bg-red-700 text-center font-bold text-white p-4 text-xs md:text-xl lg:text-2xl leading-none"
         >
           BREAKING NEWS
         </div>
-        <div class="text-center p-4 text-sm md:text-2xl lg:text-3xl">
+        <div
+          class="text-center text-sm md:text-2xl lg:text-3xl flex-grow leading-none "
+        >
           Sathyasai website under construction
-        </div>
-        <div class="flex justify-center">
-          <button
-            class="text-center text-red-500 p-4 text-xs md:text-xl lg:text-2xl"
-          >
-            X
-          </button>
         </div>
       </div>
     </div>
