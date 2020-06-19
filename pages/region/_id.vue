98dd8a36a2a7e1efef164da82d45193d3ee5d33f
Mahima Ramgolam <mahima@ramgolam.com>
2020-06-20 00:08:58 +0400

    fecth() in pages/region/_id.vue added

--- pages/region/_id.vue
+++ pages/region/_id.vue
@@ -1,39 +1,46 @@
 <template>
   <div>
     <SiteHeader />
-
-    <div class="container mx-auto">
-      <div class="region-details grid gap-1 grid-cols-2 space-y-12 py-10">
-        <div
-          class="region-title font-bold flex col-span-1 space-x-2 items-center"
-        >
-          <h1 class="font-serif text-5xl">
-            {{ myResult.name }}
-          </h1>
-        </div>
-        <div class="relative col-span-1" style="margin-top: 0;">
-          <div
-            class="absolute top-0 right-0 text-2xl text-gray-700 bg-black-100 border-2 border-black-900 rounded-full p-3 px-10 font-black"
-          >
-            Region {{ myResult.number }}
+    <div>
+      <p v-if="$fetchState.pending">Fetching posts...</p>
+      <p v-else-if="$fetchState.error">
+        Error while fetching posts: {{ $fetchState.error.message }}
+      </p>
+      <div v-else>
+        <div class="container mx-auto">
+          <div class="region-details grid gap-1 grid-cols-2 space-y-12 py-10">
+            <div
+              class="region-title font-bold flex col-span-1 space-x-2 items-center"
+            >
+              <h1 class="font-serif text-5xl">
+                {{ regions.name }}
+              </h1>
+            </div>
+            <div class="relative col-span-1" style="margin-top: 0;">
+              <div
+                class="absolute top-0 right-0 text-2xl text-gray-700 bg-black-100 border-2 border-black-900 rounded-full p-3 px-10 font-black"
+              >
+                Region {{ regions.number }}
+              </div>
+            </div>
           </div>
         </div>
+
+        <UpcomingEvents
+          :color="'gradient-bg'"
+          :textcolor="'text-blue-600'"
+          :fetchURL="
+            `/_/items/event?filter[from][gt]=now&filter[region][eq]=${regionId}`
+          "
+        />
+
+        <PastEvents />
+
+        <FeaturedPublications />
+
+        <OfficeBearer />
       </div>
     </div>
-
-    <UpcomingEvents
-      :color="'gradient-bg'"
-      :textcolor="'text-blue-600'"
-      :fetchURL="
-        `/_/items/event?filter[from][gt]=now&filter[region][eq]=${regionId}`
-      "
-    />
-
-    <PastEvents />
-
-    <FeaturedPublications />
-
-    <OfficeBearer />
 
     <SiteFooter />
   </div>
@@ -59,30 +66,24 @@ export default {
     FeaturedPublications,
     PastEvents
   },
-
+  props: ['fetchurl'],
+  // data() {
+  //   return {
+  //     regionId: this.$route.params.id,
+  //     myResult: false,
+  //     errors: false
+  //   }
+  // },
   data() {
     return {
       regionId: this.$route.params.id,
-      myResult: false,
-      errors: false
+      regions: [],
+      apiEndpoint: 'http://localhost:4444',
+      fetchURL: '/_/items/region/',
+      fetchURL2: '?fields=*.*'
     }
   },
-
   methods: {
-    getJson(response) {
-      return response.json()
-    },
-
-    displayData(result) {
-      console.log(result)
-      this.myResult = result.data
-    },
-
-    handleError(error) {
-      console.log(error)
-      this.errors = 'An error occured. Please try again later.'
-    },
-
     formatDate(param) {
       let temporaryDate = new Date(param)
       let month = temporaryDate.toLocaleString('default', { month: 'long' })
@@ -91,20 +92,54 @@ export default {
 
       let fullDate = `${month} ${day}, ${year}`
       return fullDate
-    },
-
-    fetchData() {
-      fetch(
-        'http://localhost:4444/_/items/region/' + this.regionId + '?fields=*.*'
-      )
-        .then(this.getJson)
-        .then(this.displayData)
-        .catch(this.handleError)
     }
   },
-  mounted() {
-    this.fetchData()
+
+  async fetch() {
+    let url = `${this.apiEndpoint}${this.fetchURL}${this.regionId}${this.fetchURL2}`
+    console.log(url)
+    const result = await this.$http.$get(url)
+    console.log(result)
+    this.regions = result.data
   }
+
+  // methods: {
+  //   getJson(response) {
+  //     return response.json()
+  //   },
+
+  //   displayData(result) {
+  //     console.log(result)
+  //     this.myResult = result.data
+  //   },
+
+  //   handleError(error) {
+  //     console.log(error)
+  //     this.errors = 'An error occured. Please try again later.'
+  //   },
+
+  //   formatDate(param) {
+  //     let temporaryDate = new Date(param)
+  //     let month = temporaryDate.toLocaleString('default', { month: 'long' })
+  //     let day = temporaryDate.toLocaleString('default', { day: 'numeric' })
+  //     let year = temporaryDate.toLocaleString('default', { year: 'numeric' })
+
+  //     let fullDate = `${month} ${day}, ${year}`
+  //     return fullDate
+  //   },
+
+  //   fetchData() {
+  //     fetch(
+  //       'http://localhost:4444/_/items/region/' + this.regionId + '?fields=*.*'
+  //     )
+  //       .then(this.getJson)
+  //       .then(this.displayData)
+  //       .catch(this.handleError)
+  //   }
+  // },
+  // mounted() {
+  //   this.fetchData()
+  // }
 }
 </script>
 
