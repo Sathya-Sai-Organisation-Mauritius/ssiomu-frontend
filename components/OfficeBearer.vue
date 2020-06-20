16b25f54697a945c39abfd1e554531fce193a265
Mahima Ramgolam <mahima@ramgolam.com>
2020-06-21 01:16:28 +0400

    handling error for office bearer

--- components/OfficeBearer.vue
+++ components/OfficeBearer.vue
@@ -6,46 +6,54 @@
       >
         Office Bearers
       </h2>
-      <div class="flex flex-col">
-        <div
-          class="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8"
-        >
+
+      <div>
+        <p v-if="$fetchState.pending">Fetching posts...</p>
+        <p v-else-if="$fetchState.error">
+          Error while fetching posts: {{ $fetchState.error.message }}
+        </p>
+
+        <div class="flex flex-col" v-else>
           <div
-            class="align-middle inline-block min-w-full shadow overflow-hidden border-b border-gray-200"
+            class="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8"
           >
-            <table class="min-w-full">
-              <thead>
-                <tr>
-                  <th
-                    class="px-6 py-3 border-b border-gray-200 bg-gray-100 text-left text-sm leading-4 font-bold text-gray-500 uppercase tracking-wider"
-                  >
-                    Name
-                  </th>
-                  <th
-                    class="px-6 py-3 border-b border-gray-200 bg-gray-100 text-left text-sm leading-4 font-bold text-gray-500 uppercase tracking-wider"
-                  >
-                    Title
-                  </th>
-                  <th
-                    class="px-6 py-3 border-b border-gray-200 bg-gray-100 text-left text-sm leading-4 font-bold text-gray-500 uppercase tracking-wider"
-                  >
-                    Email
-                  </th>
-                  <th
-                    class="px-6 py-3 border-b border-gray-200 bg-gray-100 text-left text-sm leading-4 font-bold text-gray-500 uppercase tracking-wider"
-                  >
-                    Role
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                <OfficeBearerBox
-                  v-for="(officeBearer, index) in members"
-                  :officebearervalues="officeBearer"
-                  :key="index"
-                />
-              </tbody>
-            </table>
+            <div
+              class="align-middle inline-block min-w-full shadow overflow-hidden border-b border-gray-200"
+            >
+              <table class="min-w-full">
+                <thead>
+                  <tr>
+                    <th
+                      class="px-6 py-3 border-b border-gray-200 bg-gray-100 text-left text-sm leading-4 font-bold text-gray-500 uppercase tracking-wider"
+                    >
+                      Name
+                    </th>
+                    <th
+                      class="px-6 py-3 border-b border-gray-200 bg-gray-100 text-left text-sm leading-4 font-bold text-gray-500 uppercase tracking-wider"
+                    >
+                      Title
+                    </th>
+                    <th
+                      class="px-6 py-3 border-b border-gray-200 bg-gray-100 text-left text-sm leading-4 font-bold text-gray-500 uppercase tracking-wider"
+                    >
+                      Email
+                    </th>
+                    <th
+                      class="px-6 py-3 border-b border-gray-200 bg-gray-100 text-left text-sm leading-4 font-bold text-gray-500 uppercase tracking-wider"
+                    >
+                      Role
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <OfficeBearerBox
+                    v-for="(officeBearer, index) in members"
+                    :officebearervalues="officeBearer"
+                    :key="index"
+                  />
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
