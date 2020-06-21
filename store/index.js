eb0d16d96739120cb2344704f83c157b03edbcba
Sandeep Ramgolam <sandeep@ramgolam.com>
2020-06-21 16:56:07 +0400

    fix links in header and footer

--- store/index.js
+++ store/index.js
@@ -1,32 +1,79 @@
 export const state = () => ({
-  list: []
+  regions: [],
+  wings: [],
+  menuResources: [
+    {
+      url: '/',
+      name: 'Sai shop'
+    },
+    {
+      url: '/',
+      name: 'Guidelines'
+    },
+    {
+      url: '/',
+      name: 'Links'
+    },
+    {
+      url: '/',
+      name: 'PWS resources'
+    },
+    {
+      url: '/',
+      name: 'Pre World Conference Videos'
+    }
+  ]
 })
 
 export const getters = {
+  getMenuResources(state) {
+    return state.menuResources
+  },
   getRegions(state) {
-    return state.list
+    let filteredRegions = state.regions.map(region => {
+      // pluck properties from object
+      let { name, number, id, slug } = region
+      return {
+        name,
+        number,
+        id,
+        slug
+      }
+    })
+    return filteredRegions
+  },
+  getWings(state) {
+    let filteredWings = state.wing.map(wing => {
+      // pluck properties from object
+      let { name, slug } = wing
+      return {
+        name,
+        slug
+      }
+    })
+    return filteredWings
   }
 }
 
 export const mutations = {
-  SET_REGIONS({ state }, payload) {
-    state.list = payload
+  SET_REGIONS(state, payload) {
+    state.regions = payload
+  },
+  SET_WINGS(state, payload) {
+    state.wing = payload
   }
 }
 
 export const actions = {
-  async GET_REGIONS({ commit }) {
-    // fetch regions
-    // let result = await this.$http.$get(
-    //   'http://localhost:4444region?fields=*.*'
-    // )
+  async nuxtServerInit({ commit }) {
+    let region = await this.$http.get('region?fields=*.*')
+    let regionData = await region.json()
 
-    let res = await fetch('http://localhost:4444region?fields=*.*')
-    let result = await res.json()
-
-    //console.log(result)
+    let wing = await this.$http.get('wing?fields=*.*')
+    let wingData = await wing.json()
 
     // store into state
-    commit('SET_REGIONS', result.data)
+    commit('SET_REGIONS', regionData.data)
+    commit('SET_WINGS', wingData.data)
   }
 }
