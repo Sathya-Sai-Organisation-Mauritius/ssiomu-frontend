0c82e476189fe62fa9e73e032da8a27a72817443
Sandeep Ramgolam <sandeep@ramgolam.com>
2020-09-29 01:20:41 +0400

    event description prose

--- pages/event/_slug.vue
+++ pages/event/_slug.vue
@@ -10,7 +10,9 @@
               <div
                 class="event-title font-bold flex md:col-span-2 space-x-2 items-center justify-center md:justify-start"
               >
-                <h1 class="font-serif text-3xl md:text-4xl lg:text-5xl">{{ events.name }}</h1>
+                <h1 class="font-serif text-3xl md:text-4xl lg:text-5xl">
+                  {{ events.name }}
+                </h1>
               </div>
               <div
                 class="relative col-span-1 mx-12 flex items-center justify-end"
@@ -21,9 +23,13 @@
                   v-if="events.region"
                 >
                   <nuxt-link
-                    :to="{name: 'region-slug', params: { slug:   events.region.slug} }"
+                    :to="{
+                      name: 'region-slug',
+                      params: { slug: events.region.slug }
+                    }"
                     class="hover:underline font-bold text-md"
-                  >Region {{ events.region.number }}</nuxt-link>
+                    >Region {{ events.region.number }}</nuxt-link
+                  >
                 </div>
               </div>
             </div>
@@ -36,40 +42,55 @@
               <div class="event-description md:text-left text-center">
                 <h2
                   class="text-3xl leading-9 tracking-tight font-extrabold text-gray-800 sm:text-4xl sm:leading-10 mb-4"
-                >Description</h2>
-                <div class="description-body space-y-2" v-html="events.description"></div>
+                >
+                  Description
+                </h2>
+                <div
+                  class="description-body space-y-2 prose"
+                  v-html="events.description"
+                ></div>
               </div>
 
               <div class="m-4 md:m-0 lg:ml-8 event-schedule">
                 <h2
                   class="text-center mb-4 text-3xl leading-9 tracking-tight font-extrabold text-gray-800 sm:text-3xl sm:leading-10"
-                >Schedule</h2>
+                >
+                  Schedule
+                </h2>
                 <div class="serif">
                   <div class="mx-12 lg:flex contact mt-4">
                     <h3
                       class="flex items-center w-50 md:w-64 lg:font-semibold justify-center text-center rounded-t-md lg:rounded-t-none lg:rounded-l-md border border-transparent bg-blue-600 text-base leading-6 font-medium text-center text-white"
-                    >From</h3>
+                    >
+                      From
+                    </h3>
 
                     <div
                       class="lg:flex font-semibold text-gray-600 rounded-b-md lg:rounded-b-none lg:rounded-r-md justify-center text-center border-2 border-gray-400 w-full"
                     >
                       <div
                         class="text-center text-xl text-gray-600 font-bold py-2"
-                      >{{ formatDate(events.to) }}</div>
+                      >
+                        {{ formatDate(events.to) }}
+                      </div>
                     </div>
                   </div>
 
                   <div class="mx-12 lg:flex contact mt-1">
                     <h3
                       class="flex items-center w-50 md:w-64 lg:font-semibold justify-center text-center rounded-t-md lg:rounded-t-none lg:rounded-l-md border border-transparent text-center bg-blue-600 text-base leading-6 font-medium text-center text-white"
-                    >To</h3>
+                    >
+                      To
+                    </h3>
 
                     <div
                       class="lg:flex font-semibold text-gray-600 rounded-b-md lg:rounded-b-none lg:rounded-r-md justify-center text-center border-2 border-gray-400 text-center w-full"
                     >
                       <div
                         class="text-center text-xl text-gray-600 font-bold py-2"
-                      >{{ formatDate(events.to) }}</div>
+                      >
+                        {{ formatDate(events.to) }}
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -83,7 +104,9 @@
             <div class="container mx-auto pt-8">
               <h2
                 class="font-serif text-center md:text-left text-white text-3xl leading-9 tracking-tight font-extrabold sm:text-4xl sm:leading-10"
-              >Gallery</h2>
+              >
+                Gallery
+              </h2>
             </div>
 
             <Gallery
