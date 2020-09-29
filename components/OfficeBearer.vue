67c55b42b548a90b1a92f5cba8bdd26c82ba54ad
Sandeep Ramgolam <sandeep@ramgolam.com>
2020-09-29 10:45:51 +0400

    hide email and phone for office bearers

--- components/OfficeBearer.vue
+++ components/OfficeBearer.vue
@@ -3,11 +3,15 @@
     <div class="region-team space-y-4">
       <h2
         class="text-3xl leading-9 tracking-tight font-extrabold text-gray-900 sm:text-4xl sm:leading-10"
-      >Office Bearers</h2>
+      >
+        Office Bearers
+      </h2>
 
       <div>
         <div class="flex flex-col">
-          <div class="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
+          <div
+            class="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8"
+          >
             <div
               class="align-middle inline-block min-w-full shadow overflow-hidden border-b border-gray-200"
             >
@@ -16,16 +20,20 @@
                   <tr>
                     <th
                       class="px-6 py-3 border-b border-gray-200 bg-gray-100 text-left text-sm leading-4 font-bold text-gray-500 uppercase tracking-wider"
-                    >Name</th>
-                    <th
+                    >
+                      Name
+                    </th>
+                    <!-- <th
                       class="px-6 py-3 border-b border-gray-200 bg-gray-100 text-left text-sm leading-4 font-bold text-gray-500 uppercase tracking-wider"
                     >Title</th>
                     <th
                       class="px-6 py-3 border-b border-gray-200 bg-gray-100 text-left text-sm leading-4 font-bold text-gray-500 uppercase tracking-wider"
-                    >Email</th>
+                    >Email</th> -->
                     <th
                       class="px-6 py-3 border-b border-gray-200 bg-gray-100 text-left text-sm leading-4 font-bold text-gray-500 uppercase tracking-wider"
-                    >Role</th>
+                    >
+                      Role
+                    </th>
                   </tr>
                 </thead>
                 <tbody>
--- components/OfficeBearerBox.vue
+++ components/OfficeBearerBox.vue
@@ -2,16 +2,18 @@
   <tr class="bg-white">
     <td
       class="px-6 py-4 whitespace-no-wrap text-lg leading-5 font-medium text-gray-900"
-    >{{ officebearervalues.name }}</td>
-    <td
+    >
+      {{ officebearervalues.name }}
+    </td>
+    <!-- <td
       class="px-6 py-4 whitespace-no-wrap text-lg leading-5 text-gray-500"
     >{{ officebearervalues.phone }}</td>
     <td
       class="px-6 py-4 whitespace-no-wrap text-lg leading-5 text-gray-500"
-    >{{ officebearervalues.email }}</td>
-    <td
-      class="px-6 py-4 whitespace-no-wrap text-lg leading-5 text-gray-500"
-    >{{ officebearervalues.role }}</td>
+    >{{ officebearervalues.email }}</td> -->
+    <td class="px-6 py-4 whitespace-no-wrap text-lg leading-5 text-gray-500">
+      {{ officebearervalues.role }}
+    </td>
   </tr>
 </template>
 
