e9c47f168d9fed32ee8b0f98b3d5c9c6cbb91768
Mahima Ramgolam <mahima@ramgolam.com>
2020-09-30 16:13:59 +0400

    pages.php rsponsive updated

--- pages/page/_slug.vue
+++ pages/page/_slug.vue
@@ -2,9 +2,11 @@
   <div>
     <ErrorHandler :model="pages">
       <div class="container mx-auto " v-if="pages">
-        <div class=" page-details space-y-12 py-20 text-center">
+        <div
+          class=" page-details space-y-10 md:space-y-12 py-12 md:py-20 text-center"
+        >
           <div class="page-title font-bold space-x-2 items-center">
-            <h1 class="font-serif text-6xl text-center">
+            <h1 class="font-serif text-3xl md:text-4xl lg:text-6xl text-center">
               {{ pages.title }}
             </h1>
           </div>
